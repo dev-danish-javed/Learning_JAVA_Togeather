c75dd00e50697086c987f328dfbe4b87ed33eb9e
Danish Javed <danishjaved636@gmail.com>
2023-03-11 00:22:15 +0530

    Initialized with Exception Handling

---
+++ src/exceptionHanlding/TryCatchTest.java
@@ -0,0 +1,16 @@
+package exceptionHanlding;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TryCatchTest {
+    public static void main(String[] args) {
+        String[] names = new String[10];
+        System.out.println(names[9]);
+        int a = 10 / 0;
+        Path path = Paths.get("src/main/resources/shakespeare.txt");
+        List contents = Files.readAllLines(path);
+    }
+}
